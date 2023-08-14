a47a72017ba9e2d445fd0c981fc49f5d4147f081
Natalia Trelak <natalia@dascompany.com>
2023-08-14 14:54:49 +0200

    Added empty lines

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 const story = await useStoryblok('home', { 
-  version: 'draft',
+  version: useRoute().query._storyblok ? "draft" : "published",
   resolve_relations: 'featuredProjects.project'
 },{
   resolveRelations: 'featuredProjects.project'
@@ -8,6 +8,5 @@ const story = await useStoryblok('home', {
 </script>
 
 <template>
-  <StoryblokComponent v-if="story" :blok="story.content"> </StoryblokComponent>
-
+  <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
 </template>
